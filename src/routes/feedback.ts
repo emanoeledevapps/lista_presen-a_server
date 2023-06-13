a50883aba02a028002f0f256771822bf8f84551a
Emanoel Augusto Freires <emanoelaugusto7@gmail.com>
2023-06-13 18:26:32 -0300

    add routes feedback

---
+++ src/routes/feedback.ts
@@ -0,0 +1,40 @@
+import {FastifyInstance} from 'fastify';
+import { prisma } from '../lib/prisma';
+import {z} from 'zod';
+import { authenticated } from '../plugins/authenticated';
+
+export async function feedbackRoutes(fastify: FastifyInstance){
+    fastify.post('/feedback',{onRequest: [authenticated]}, async (request, reply) => {
+        const createFeedbackProps = z.object({
+            title: z.string(),
+            description: z.string(),
+            wallet: z.string(),
+            photoHash: z.string(),
+
+        });
+    
+        const {title, description, wallet, photoHash} = createFeedbackProps.parse(request.body);
+    
+        await prisma.feedback.create({
+            data:{
+                title,
+                description,
+                wallet,
+                photoHash
+            }
+        })
+    
+        return reply.status(201).send();
+    })
+
+
+    fastify.get('/feedback', async (request, reply) => {
+        const feedbacks = await prisma.feedback.findMany({
+            orderBy:{
+                createdAt: 'desc'
+            }
+        })
+
+        return {feedbacks}
+    })
+}
--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ import { authRoutes } from './routes/auth';
 import { blogRoutes } from './routes/blog';
 import { impactRoutes } from './routes/impact';
 import { tokensRoutes } from './routes/tokens';
+import { feedbackRoutes } from './routes/feedback';
 
 const app = fastify();
 app.register(cors, {
@@ -21,6 +22,7 @@ app.register(authRoutes);
 app.register(blogRoutes);
 app.register(impactRoutes);
 app.register(tokensRoutes);
+app.register(feedbackRoutes);
 
 app.register(jwt, {
     secret: process.env.JWT_SECRET_KEY || '123456'
